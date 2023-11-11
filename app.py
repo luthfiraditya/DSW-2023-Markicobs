8d087f9042ddca6435f4a6f2b1e066feb67aed41
luthfirdty <luthfirdty@gmail.com>
2023-11-11 14:34:54 +0700

    model adaptation

--- app.py
+++ app.py
@@ -2,72 +2,66 @@ import streamlit as st
 import pandas as pd
 import pickle
 
-# Load the Random Forest model from the pickle file
-model = pickle.load(open('randomforest.pkl', 'rb'))
+model_low = pickle.load(open('model_low.pkl', 'rb'))
+model_mid = pickle.load(open('model_mid.pkl', 'rb'))
+model_high = pickle.load(open('model_high.pkl', 'rb'))
 
-# Define the columns for user input
-columns = ['tenure', 'PhoneService', 'Contract',
-           'PaperlessBilling', 'PaymentMethod', 'MonthlyCharges']
+columns_low = ['Tenure Months', 'Payment Method']
+columns_mid = ['Total Expense (Thou. IDR)', 'Behaviour_Combination']
+columns_high = ['Total Expense (Thou. IDR)', 'Behaviour_Combination']
 
-# Create a function to preprocess user input and make predictions
-
-
-def predict_churn(input_data):
-    # Preprocess the input data
+def predict_churn(model, columns, input_data):
     input_df = pd.DataFrame([input_data], columns=columns)
 
-    # Make predictions using the loaded model
     prediction = model.predict(input_df)
     probability = model.predict_proba(input_df)[:, 1]
 
     return prediction[0], probability[0]
 
-# Create the Streamlit app
-
-
 def main():
     st.title("Telecom Churn Prediction Markicobss")
     st.write("Enter the customer details below to predict churn.")
 
-    # Create input fields for user input
-    tenure = st.slider("Tenure (months)", 0, 100, 1)
+    device_type = st.radio("Select your device type:", ['Low', 'Mid', 'High'])
+
+    if device_type == 'Low':
+        model = model_low
+        columns = columns_low
+
+        tenure = st.slider("Tenure Months", 0, 100, 1)
+        payment_method = st.selectbox("Payment Method", [0, 1, 2, 3], format_func=lambda x: ['Credit', 'Debit', 'Digital Wallet', 'Pulsa'][x])
+        
+        input_data = {
+            'Tenure Months': tenure,
+            'Payment Method': payment_method
+        }
+
+    elif device_type == 'Mid' or device_type == 'High':
+        if device_type == 'Mid':
+            model = model_mid
+            columns = columns_mid
+        else:
+            model = model_high
+            columns = columns_high
+
+        total_expense = st.number_input("Total Expense (Thou. IDR)")
+        behavior_combination = st.selectbox("Behaviour Combination", [0, 1, 2], format_func=lambda x: ['Option 1', 'Option 2', 'Option 3'][x])
+        
+        input_data = {
+            'Total Expense (Thou. IDR)': total_expense,
+            'Behaviour_Combination': behavior_combination
+        }
+
+    churn_prediction, churn_probability = predict_churn(model, columns, input_data)
     
-    phone_service = st.selectbox("Phone Service", [0, 1])
-    st.write("0: No, 1: Yes")
-    contract = st.selectbox("Contract", [0, 1, 2])
-    st.write("0: Month-to-month, 1: One year, 2: Two year")
-    paperless_billing = st.selectbox("Paperless Billing", [0, 1])
-    st.write("0: No, 1: Yes")
-    payment_method = st.selectbox("Payment Method", [0, 1, 2, 3])
-    st.write("0: Bank transfer (automatic), 1: Credit card (automatic), 2: Electronic check, 3: Mailed check")
-    monthly_charges = st.number_input("Monthly Charges")
-
-    # Create a dictionary to store the user input
-    input_data = {
-        'tenure': tenure,
-        'PhoneService': phone_service,
-        'Contract': contract,
-        'PaperlessBilling': paperless_billing,
-        'PaymentMethod': payment_method,
-        'MonthlyCharges': monthly_charges
-    }
-
-    # Predict churn based on user input
-    churn_probability = predict_churn(input_data)
-    churn_prediction=churn_probability[1]
-    # Display the prediction
     st.subheader("Churn Prediction")
     if churn_prediction >= 0.4:
         st.write("The customer is likely to churn.")
     else:
         st.write("The customer is unlikely to churn.")
 
-    # Display the churn probability
     st.subheader("Churn Probability")
-
     st.write("The probability of churn is:", churn_probability)
 
-
-# Run the Streamlit app
 if __name__ == '__main__':
     main()
