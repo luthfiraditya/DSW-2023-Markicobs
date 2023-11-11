1527eea92d4fa73d185b92089aadfb3c75143618
luthfirdty <luthfirdty@gmail.com>
2023-11-12 00:11:24 +0700

    fixing the combination issue

--- app.py
+++ app.py
@@ -1,22 +1,70 @@
 import streamlit as st
 import pandas as pd
-import pickle
+import pickle 
+import numpy as np
+
 
 model_low = pickle.load(open('model_low.pkl', 'rb'))
 model_mid = pickle.load(open('model_mid.pkl', 'rb'))
 model_high = pickle.load(open('model_high.pkl', 'rb'))
 
 columns_low = ['Tenure Months', 'Payment Method']
-columns_mid = ['Total Expense (Thou. IDR)', 'Behaviour_Combination']
-columns_high = ['Total Expense (Thou. IDR)', 'Behaviour_Combination']
+columns_mid=['Total Expense (Thou. IDR)', 'Behaviour_Combination']
+columns_high=['Total Expense (Thou. IDR)', 'Behaviour_Combination']
+
+df_low = pd.read_excel('lowend_result.xlsx')
+df_mid = pd.read_excel('midend_result.xlsx')
+df_high = pd.read_excel('highend_result.xlsx')
+
+
+def get_user_combination(user_input,df_combi):
+    # Assuming df_mid is your DataFrame
+    # Replace this with the actual DataFrame and column names
+
+    # Condition to filter the DataFrame based on user input
+    condition = (
+        (df_combi['Games Product'] == user_input['Games Product']) &
+        (df_combi['Music Product'] == user_input['Music Product']) &
+        (df_combi['Education Product'] == user_input['Education Product']) &
+        (df_combi['Call Center'] == user_input['Call Center']) &
+        (df_combi['Use MyApp'] == user_input['Use MyApp']) &
+        (df_combi['Payment Method'] == user_input['Payment Method']) &
+        (df_combi['Video Product'] == user_input['Video Product'])
+    )
+
+    # Filter the DataFrame
+    filtered_df = df_combi.loc[condition]
+
+    # Check if any rows match the condition
+    if not filtered_df.empty:
+        # Retrieve the 'Combination' value
+        result_combination = filtered_df['Combination'].iloc[0]
+        result_combination=int(result_combination.split()[-1])
+        return result_combination
+    else:
+        return "No matching combination"
+
 
 def predict_churn(model, columns, input_data):
     input_df = pd.DataFrame([input_data], columns=columns)
 
     prediction = model.predict(input_df)
-    probability = model.predict_proba(input_df)[:, 1]
+    probability = model.predict_proba(input_df)
+    max_probabilities = np.max(probability, axis=1)
 
-    return prediction[0], probability[0]
+    if prediction[0]==0 :
+        if 0.5<max_probabilities<0.91:
+            churn_risk="Low"
+        else:
+            churn_risk="least concern"
+    else:
+        if 0.5<max_probabilities<0.54:
+            churn_risk="Notable"
+        else:
+            churn_risk="Threatening"
+
+
+    return prediction[0], probability[0], churn_risk
 
 def main():
     st.title("Telecom Churn Prediction Markicobss")
@@ -45,23 +93,48 @@ def main():
             columns = columns_high
 
         total_expense = st.number_input("Total Expense (Thou. IDR)")
-        behavior_combination = st.selectbox("Behaviour Combination", [0, 1, 2], format_func=lambda x: ['Option 1', 'Option 2', 'Option 3'][x])
         
-        input_data = {
-            'Total Expense (Thou. IDR)': total_expense,
-            'Behaviour_Combination': behavior_combination
+        Games_Product = st.selectbox("Games Product", ['Yes', 'No'])
+        Music_Product = st.selectbox("Music Product", ['Yes', 'No'])
+        Education_Product = st.selectbox("Education Product", ['Yes', 'No'])
+        Call_Center = st.selectbox("Call Center", ['Yes', 'No'])
+        Use_MyApp = st.selectbox("Use MyApp", ['Yes', 'No'])
+        Payment_Method = st.selectbox("Payment Method", ['Pulsa', 'Digital Wallet', 'Debit', 'Credit'])
+        Video_Product = st.selectbox("Video Product", ['Yes', 'No'])
+
+        user_behavior = {
+            'Games Product': Games_Product,
+            'Music Product': Music_Product,
+            'Education Product': Education_Product,
+            'Call Center': Call_Center,
+            'Use MyApp': Use_MyApp,
+            'Payment Method': Payment_Method,
+            'Video Product': Video_Product
         }
 
-    churn_prediction, churn_probability = predict_churn(model, columns, input_data)
+        behavior_combination=get_user_combination(user_behavior,df_mid)
+
+        input_data = {
+        'Total Expense (Thou. IDR)':total_expense,'Behaviour_Combination':behavior_combination}
+       
+
+    churn_prediction, churn_probability,churn_risk = predict_churn(model, columns, input_data)
     
     st.subheader("Churn Prediction")
-    if churn_prediction >= 0.4:
-        st.write("The customer is likely to churn.")
-    else:
+    if churn_prediction==0:
         st.write("The customer is unlikely to churn.")
+    else:
+        st.write("The customer is likely to churn.")
 
-    st.subheader("Churn Probability")
-    st.write("The probability of churn is:", churn_probability)
+    
+    max_prob=np.max(churn_probability)*100
 
+    st.subheader("Churn probability")
+    st.write("The probability of churn is:", round(max_prob,2))
+
+
+    st.subheader("Churn Risk")
+    st.write("The Churn risk of customer is:", churn_risk)
+    
 if __name__ == '__main__':
     main()
